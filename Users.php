46a5b4f3f573160c1d3d7d6d4de8970cd4655e85
Noe Huaccharaque <noemk2@gmail.com>
2019-09-15 14:51:07 -0500

    withServiceAccount or ServoceAccount

---
+++ Users.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once './vendor/autoload.php';
+
+use Kreait\Firebase\Factory;
+// use Kreait\Firebase\ServiceAccount;
+
+class Users
+{
+    public function __construct()
+    {
+        // $acc = withServiceAccount('./secret/php-tutorial-640b0-f517fd37fac9.json');
+        // $firebase = (new Factory)->$acc->create();
+    }
+}
