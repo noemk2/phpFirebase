3a64d027df83e7ebdd63b1c140e27a89d2f74b79
Noe Huaccharaque <noemk2@gmail.com>
2019-09-15 15:51:04 -0500

    problems and problems

--- index.php
+++ index.php
@@ -1,18 +1,11 @@
 <?php
 require_once './vendor/autoload.php';
-// require __DIR__ . '/vendor/autoload.php';
 
 use Kreait\Firebase\Factory;
-// use Kreait\Firebase\ServiceAccount;
-
 
 $firebase = (new Factory)
     ->withServiceAccount('./secret/php-tutorial-640b0-f517fd37fac9.json')
-    // ->withServiceAccount::fromJsonFile(__DIR__ . '/secret/php-tutorial-640b0-f517fd37fac9.json')
-    // The following line is optional if the project id in your credentials file
-    // is identical to the subdomain of your Firebase project. If you need it,
-    // make sure to replace the URL with the URL of your project.
-    // ->withDatabaseUri('https://php-tutorial-640b0.firebaseio.com/')
+    ->withDatabaseUri('https://php-tutorial-640b0.firebaseio.com/')
     ->create();
 
 $database = $firebase->getDatabase();
